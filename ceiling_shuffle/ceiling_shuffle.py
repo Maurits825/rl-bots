6865e3ebe5b13ba6551657454e63021c83883931
Maurits van der Steur <mauritsvdsteur@live.com>
2018-10-27 19:14:25 +0100

    added setup sates

--- ceiling_shuffle/ceiling_shuffle.py
+++ ceiling_shuffle/ceiling_shuffle.py
@@ -24,17 +24,25 @@ class CeilingShuffle(BaseAgent):
 
         # ceiling shuffle
         self.ceiling_shuffle()
+        if self.current_state == 'Idle':
+            self.current_state = 'Reset'
 
         #render stuff
         self.renderer.begin_rendering()
         self.renderer.draw_string_2d(0, 0, 5, 5, self.current_state,
+                                     self.renderer.black())
+        self.renderer.draw_string_2d(0, 100, 5, 5, str(self.me.pos.x),
+                                     self.renderer.black())
+        self.renderer.draw_string_2d(0, 200, 5, 5, str(self.me.pos.y),
+                                     self.renderer.black())
+        self.renderer.draw_string_2d(0, 300, 5, 5, str(self.me.pos.z),
                                      self.renderer.black())
         self.renderer.end_rendering()
 
         return self.controller
 
     def preprocess(self, game):
-        index = 0
+        index = self.index
         self.me.pos.x = game.game_cars[index].physics.location.x
         self.me.pos.y = game.game_cars[index].physics.location.y
         self.me.pos.z = game.game_cars[index].physics.location.z
@@ -72,22 +80,59 @@ class CeilingShuffle(BaseAgent):
         self.ball.isBall = True
 
     def ceiling_shuffle(self):
-        if self.current_state == 'Middle':
+        if self.current_state == 'Reset':
+            # aim at orange goal and throttle
+            target_y = 3200
+            self.controller.steer = aim_front(self.me, Vector3(0, target_y, 0))
+            self.controller.throttle = 0.5
+
+            # check if at orange goal
+            offset = 200
+            if (-offset < self.me.pos.x < offset and
+                    (target_y-offset) < self.me.pos.y < (target_y+offset)):
+                self.next_state = 'Middle'
+
+        elif self.current_state == 'Middle':
             # aim at middle and throttle
-            self.controller.steer = (self.me, Vector3(0, 0, 0))
+            self.controller.steer = aim_front(self.me, Vector3(0, 0, 0))
             self.controller.throttle = 1
 
             # check if at center
             offset = 50
             if (-offset < self.me.pos.x < offset and
                     -offset < self.me.pos.y < offset):
+                self.controller.throttle = -1
                 self.next_state = 'Corner'
 
         elif self.current_state == 'Corner':
-            # go corner
-            pass
+            # aim at corner and throttle
+            t_x = 3072
+            t_y = -4096
+            self.controller.steer = aim_front(self.me, Vector3(t_x, t_y, 0))
+            self.controller.throttle = 1
+
+            # check if at center
+            offset = 50
+            if ((t_x-offset) < self.me.pos.x < (t_x+offset) and
+                    (t_y-offset) < self.me.pos.y < (t_y+offset)):
+                self.controller.throttle = -1
+                self.next_state = 'Roof'
+
         elif self.current_state == 'Roof':
             # go to roof
-            pass
+            # should be facing correct direction
+            self.controller.steer = 0
+            self.controller.throttle = 1
+
+            # check if at roof
+            ceiling_z = 2044
+            offset = 50
+            if self.me.pos.z > ceiling_z - offset:
+                self.controller.throttle = -1
+                self.next_state = 'Middle'
+
+        elif self.current_state == 'Idle':
+            self.controller.steer = 0
+            self.controller.throttle = 0
 
         self.current_state = self.next_state
