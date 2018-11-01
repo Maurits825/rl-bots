47d1586bdfd01e0305cb21896535a683ba6dc419
Maurits van der Steur <mauritsvdsteur@live.com>
2018-11-01 18:17:27 +0000

    cleaned up states, added preprocess

--- half_flip/half_flip.py
+++ half_flip/half_flip.py
@@ -2,6 +2,8 @@ from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 import math
 import time
+from utility.util import *
+from rlbot.utils.game_state_util import GameState
 
 
 class HalfFlip(BaseAgent):
@@ -12,52 +14,29 @@ class HalfFlip(BaseAgent):
         # Contants
         self.DODGE_TIME = 0.2
 
-        # Game values
-        self.bot_pos = None
-        self.bot_yaw = None
-        self.bot_pitch = 0
-        self.bot_roll = 0
+        self.me = Obj()
+        self.ball = Obj()
 
-        self.current_state = 'N'
-        self.next_state = 'N'
+        self.current_state = 'Idle'
+        self.next_state = 'Idle'
         self.jump_start = 0
 
-        # This is just a variable used to make the bot jump every few seconds as a demonstration.
-        # This isn't used for anything else, so you can remove it (and the code block that contains this
-        # variable (line 68-ish)) if you don't want to see the bot jump every few seconds
-        self.dodge_interval = 0
-
-    def aim(self, target_x, target_y):
-        angle_between_bot_and_target = math.atan2(target_y - self.bot_pos.y,
-                                                target_x - self.bot_pos.x)
-
-        angle_front_to_target = angle_between_bot_and_target - self.bot_yaw
-
-        # Correct the values
-        if angle_front_to_target < -math.pi:
-            angle_front_to_target += 2 * math.pi
-        if angle_front_to_target > math.pi:
-            angle_front_to_target -= 2 * math.pi
-
-        if angle_front_to_target < math.radians(-10):
-            # If the target is more than 10 degrees right from the centre, steer left
-            self.controller.steer = -1
-        elif angle_front_to_target > math.radians(10):
-            # If the target is more than 10 degrees left from the centre, steer right
-            self.controller.steer = 1
-        else:
-            # If the target is less than 10 degrees from the centre, steer straight
-            self.controller.steer = 0
-
     def half_flip(self):
-        if self.current_state == 'N':
-            self.controller.throttle = 0.2
+        if self.current_state == 'Idle':
+            self.controller.throttle = 0
             self.controller.jump = 0
             self.controller.roll = 0
             self.controller.pitch = 0
-            self.next_state = 'N'
+            self.next_state = 'Idle'
 
-        elif self.current_state == 'start':
+        elif self.current_state == 'Reset':
+            self.controller.throttle = 0
+            self.controller.jump = 0
+            self.controller.roll = 0
+            self.controller.pitch = 0
+            self.next_state = 'Start'
+
+        elif self.current_state == 'Start':
             self.controller.jump = 1
             self.controller.pitch = 1
             self.jump_start = time.time()
@@ -73,60 +52,79 @@ class HalfFlip(BaseAgent):
         elif self.current_state == 'second_jump':
             self.controller.jump = 1
             self.controller.pitch = 1
-            self.next_state = 'wait_cancel'
-
-        elif self.current_state == 'wait_cancel':
-            self.controller.jump = 0
-            self.controller.roll = 0
-            if self.bot_pitch > 1.4:
+            if self.me.rotation.x > 1.4:
                 self.next_state = 'cancel'
             else:
-                self.next_state = 'wait_cancel'
+                self.next_state = 'second_jump'
 
         elif self.current_state == 'cancel':
             self.controller.pitch = -1
-            self.next_state = 'wait_roll'
-
-        elif self.current_state == 'wait_roll':
-            if self.bot_pitch < 0.3:
+            if self.me.rotation.x < 0.3:
                 self.next_state = 'roll'
             else:
-                self.next_state = 'wait_roll'
+                self.next_state = 'cancel'
 
         elif self.current_state == 'roll':
             self.controller.roll = 1
-            self.next_state = 'wait_N'
-
-        elif self.current_state == 'wait_N':
-            if self.bot_roll < 0.1 and self.bot_roll > -0.1:
-                self.next_state = 'N'
+            if -0.1 < self.me.rotation.z < 0.1:
+                self.next_state = 'Idle'
             else:
-                self.next_state = 'wait_N'
+                self.next_state = 'roll'
 
         else:
-            self.next_state = 'N'
+            self.next_state = 'Idle'
 
         self.current_state = self.next_state
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         # Update game data variables
-        self.bot_yaw = packet.game_cars[self.team].physics.rotation.yaw
-        self.bot_pos = packet.game_cars[self.index].physics.location
-        self.bot_pitch = packet.game_cars[self.team].physics.rotation.pitch
-        self.bot_roll = packet.game_cars[self.team].physics.rotation.roll
+        self.preprocess(packet)
 
-        # Just making the bot jump every 3 seconds as demonstration. You can remove this if-block and it won't break the code.
-        if self.dodge_interval < time.time():
-            #self.should_dodge = True
-            self.current_state = 'start'
-            self.dodge_interval = time.time() + 3
+        if packet.game.game_cars[0].jumped:
+            self.next_state = 'Reset'
+            self.half_flip()
 
-        self.controller.throttle = -0.2
-        self.half_flip()
-
-        #render stuff
+        # render stuff
         self.renderer.begin_rendering()
         self.renderer.draw_string_2d(0, 0, 5, 5, self.current_state, self.renderer.black())
         self.renderer.end_rendering()
 
         return self.controller
+
+    def preprocess(self, game):
+        index = self.index
+        self.me.pos.x = game.game_cars[index].physics.location.x
+        self.me.pos.y = game.game_cars[index].physics.location.y
+        self.me.pos.z = game.game_cars[index].physics.location.z
+
+        self.me.velocity.x = game.game_cars[index].physics.velocity.x
+        self.me.velocity.y = game.game_cars[index].physics.velocity.y
+        self.me.velocity.z = game.game_cars[index].physics.velocity.z
+
+        self.me.rotation.x = game.game_cars[index].physics.rotation.pitch
+        self.me.rotation.y = game.game_cars[index].physics.rotation.yaw
+        self.me.rotation.z = game.game_cars[index].physics.rotation.roll
+
+        self.me.rvel.x = game.game_cars[index].physics.angular_velocity.x
+        self.me.rvel.y = game.game_cars[index].physics.angular_velocity.y
+        self.me.rvel.z = game.game_cars[index].physics.angular_velocity.z
+
+        self.ball.pos.x = game.game_ball.physics.location.x
+        self.ball.pos.y = game.game_ball.physics.location.y
+        self.ball.pos.z = game.game_ball.physics.location.z
+
+        self.ball.velocity.x = game.game_ball.physics.velocity.x
+        self.ball.velocity.y = game.game_ball.physics.velocity.y
+        self.ball.velocity.z = game.game_ball.physics.velocity.z
+
+        self.ball.rotation.x = game.game_ball.physics.rotation.pitch
+        self.ball.rotation.y = game.game_ball.physics.rotation.yaw
+        self.ball.rotation.z = game.game_ball.physics.rotation.roll
+
+        self.ball.rvel.x = game.game_ball.physics.angular_velocity.x
+        self.ball.rvel.y = game.game_ball.physics.angular_velocity.y
+        self.ball.rvel.z = game.game_ball.physics.angular_velocity.z
+
+        #self.me.matrix = rotator_to_matrix(self.me)
+
+        self.ball.isBall = True
