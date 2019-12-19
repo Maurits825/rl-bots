680256968f1178336ebd859ef9a2ab7bdccf6e5d
Maurits van der steur <mauritsvdsteur@live.com>
2019-12-19 18:24:24 +0400

    Updated predict bot, added testbot as template example

--- PredictBot/PredictBot.py
+++ PredictBot/PredictBot.py
@@ -1,15 +1,23 @@
 import math
 import time
+import sys
+import os
+
+# in order to access utility folder
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
+
 from utility.util import *
 from utility.predict import *
 
 
-class Predict(BaseAgent):
-    def __init__(self, name, team, index):
-        super().__init__(name, team, index)
-        self.controller = SimpleControllerState()
+class PredictBot(BaseAgent):
+
+    def initialize_agent(self):
+        # This runs once before the bot starts up
+        self.controller_state = SimpleControllerState()
         self.ball = Obj()
         self.me = Obj()
         self.get_ball_prediction_struct()
@@ -18,15 +26,14 @@ class Predict(BaseAgent):
         # preprocess game data variables
         self.preprocess(packet)
 
-        print('hello')
         # render stuff
         t = time_to_ground(self.ball)
         self.renderer.begin_rendering()
-        self.draw_curve(self.me)
+        self.draw_curve(self.ball)
         self.renderer.end_rendering()
-        return self.controller
+        return self.controller_state
 
-    def preprocess(self, game):
+    def preprocess(self, game):  # TODO move to utils?
         index = 0
         self.me.pos.x = game.game_cars[index].physics.location.x
         self.me.pos.y = game.game_cars[index].physics.location.y
@@ -68,7 +75,7 @@ class Predict(BaseAgent):
         self.renderer.draw_rect_3d(ground_pos, 20, 20, True,
                                    self.renderer.red())
 
-        samples = 8
+        samples = 15
         prev_pos = pos_at_time(obj, 0)
         for i in range(1, samples+1):
             j = t*i/samples
