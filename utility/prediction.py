8cb91fd51a3bca76d186aebaa13c7d53ab6a9436
Maurits van der steur <mauritsvdsteur@live.com>
2019-12-22 17:06:41 +0400

    fixed/workaround for import statements, cleanup and fixed drag equations, added flag for physics method

--- utility/prediction.py
+++ utility/prediction.py
@@ -1,10 +1,29 @@
 import math
+import os
+import sys
 from utility.myVec3 import MyVec3
-from rlbot.utils.game_state_util import Physics
+
+try:
+    from scipy.special import lambertw
+except ImportError:
+    sys.path.insert(1, os.path.join(sys.path[0], r"C:\Users\Maurits\AppData\Local\Programs\Python\Python38\Lib\site-packages\scipy"))
+    from scipy.special import lambertw
+
+try:
+    from rlbot.utils.game_state_util import Physics
+except ModuleNotFoundError:
+    sys.path.insert(1, os.path.join(sys.path[0], r"C:\Users\Maurits\AppData\Local\RLBotGUI\Python\Lib\site-packages"))
+    from rlbot.utils.game_state_util import Physics
+
+# TODO problems with sci py lib, this cmd worked for rl bot env:
+# "C:\Users\Maurits\AppData\Local\RLBotGUI\Python\python.exe" -m pip install --target="C:\Users\Maurits\AppData\Local\RLBotGUI\Python\Lib\site-packages" scipy
+# it defauls try to use that sci py, when running in diff env like when running rl-bot-sim
+# is this messy?
+
 
 # ref: https://github.com/RLBot/RLBot/wiki/Useful-Game-Values
 G = -650
-DRAG = 3/100
+DRAG = 0.030500000342726707
 BALL_RADIUS = 92.75
 CAR_HEIGHT = 17.01  # octane
 BOOST_ACC = 430 #TODO fix this
@@ -17,34 +36,37 @@ B = math.log(1-DRAG) / FPS
 
 Y = 2.0
 mu = 0.285
-C_R = 0.6
+C_R = 0.6000000238418579
 F = 0.0003
 
+moment_of_inertia = 99918.75
+mass = 30
+omega_max = 6
 
-# Following functions are based on frame number (f)
-def d_get_vel_z(f, initial_velocity_z):
-    return ((initial_velocity_z - A) * math.e**(DECAY*f)) + A
+# Following functions are based on frame number (f), TODO change based on time?
+def d_get_vel(f, initial_velocity):
+    c = (initial_velocity.z - A) / -DECAY
 
-
-def d_get_pos_z(f, initial_velocity_z, initial_pos_z):
-    c = (initial_velocity_z - A) / -DECAY
-    return initial_pos_z + (((((initial_velocity_z - A) * math.e**(DECAY*f)) / DECAY) + A*f + c) / FPS)
-
-
-def d_get_vel_x(f, initial_velocity_x):
-    return initial_velocity_x * (1-DRAG)**(f / FPS)
+    return MyVec3(initial_velocity.x * (1-DRAG)**(f / FPS),
+                  initial_velocity.y * (1-DRAG)**(f / FPS),
+                  ((initial_velocity.z - A) * math.e ** (DECAY * f)) + A)
 
 
-def d_get_vel_y(f, initial_velocity_y):
-    return d_get_vel_x(f, initial_velocity_y)
+def d_get_pos(f, initial_velocity, initial_position):
+    c = (initial_velocity.z - A) / -DECAY
+
+    return MyVec3(initial_position.x + ((((initial_velocity.x * (1 - DRAG)**(f/FPS)) / B) - (initial_velocity.x / B)) / FPS),
+                  initial_position.y + ((((initial_velocity.y * (1 - DRAG)**(f/FPS)) / B) - (initial_velocity.y / B)) / FPS),
+                  initial_position.z + (((((initial_velocity.z - A) * math.e ** (DECAY * f)) / DECAY) + A * f + c) / FPS))
 
 
-def d_get_pos_x(f, initial_velocity_x, initial_pos_x):
-    return initial_pos_x + ((((initial_velocity_x * (1 - DRAG)**(f/FPS)) / B) - (initial_velocity_x / B)) / FPS)
+def d_time_to_ground(initial_velocity, initial_position):
+    K = (initial_velocity.z - A) / DECAY
+    const_A = (K - FPS*(initial_position.z - BALL_RADIUS)) / A
+    W = lambertw((DECAY*K * math.e**(DECAY * const_A)) / A)
 
-
-def d_get_pos_y(f, initial_velocity_y, initial_pos_y):
-    return d_get_pos_x(f, initial_velocity_y, initial_pos_y)
+    ret = const_A - (W / DECAY)
+    return ret.real / FPS
 
 
 def time_to_ground(velocity: MyVec3, position: MyVec3, is_ball=True):
@@ -73,25 +95,38 @@ def get_pos_at_time(velocity: MyVec3, position: MyVec3, t):
     y = position.y
     z = position.z
 
-    return MyVec3((vx*t + 0.5*vx*t**2) + x,
-            (vy*t + 0.5*vx*t**2) + y,
-            (vz*t + 0.5*G*t**2) + z)
+    return MyVec3((vx*t) + x,
+                  (vy*t) + y,
+                  (vz*t + 0.5*G*t**2) + z)
 
 
-def get_vel_z(t, initial_velocity_z):
-    return initial_velocity_z + G*t
+def get_vel_at_time(t, initial_velocity):
+    return MyVec3(initial_velocity.x,
+                  initial_velocity.y,
+                  initial_velocity.z + G*t)
 
 
-def update_physics_after_bounce(physics: Physics, time_ground, ground_pos):
+# ref: https://samuelpmish.github.io/notes/RocketLeague/ball_bouncing/
+def update_physics_after_bounce(physics: Physics, time_ground, ground_pos, physics_flag):
     ret_physics = Physics()
 
-    vz = get_vel_z(time_ground, physics.velocity.z)
-    ground_velocity = MyVec3(physics.velocity.x,
-                             physics.velocity.y,
-                             vz)
+    ground_velocity = None
+
+    if physics_flag == 'no_drag':
+        ground_velocity = get_vel_at_time(time_ground, physics.velocity)
+
+    elif physics_flag == 'drag':
+        frame = time_ground * FPS
+        ground_velocity = d_get_vel(frame, physics.velocity)
+
+    elif physics_flag == 'engine_sim':
+        pass
+
+    else:
+        return
 
     normal = MyVec3(0, 0, 1)  # TODO assume ground bounces for now
-    vel_perp = normal.dot(ground_velocity) * normal  # velocity projection on normal vector
+    vel_perp = ground_velocity.dot(normal) * normal  # velocity projection on normal vector
     vel_para = ground_velocity - vel_perp  # vector subtraction to get parallel vector
     vel_spin = BALL_RADIUS * normal.cross(physics.angular_velocity)  # spin velocity is perp to normal and axis of ang v
     vel_slip = vel_para + vel_spin
@@ -99,41 +134,86 @@ def update_physics_after_bounce(physics: Physics, time_ground, ground_pos):
     ratio = vel_perp.mag() / vel_slip.mag()
 
     delta_vel_perp = - (1 + C_R) * vel_perp
-    delta_vel_para = - min(1, Y * ratio) * mu * vel_slip
+    delta_vel_para = -1 * (min(1, Y * ratio) * mu * vel_slip)
+
+    delta_vel_perp_m = (-mass * (1 + C_R) * vel_perp) * (1/FPS)
+    delta_vel_para_m = (-mass * (min(1, Y * ratio) * mu * vel_slip)) * (1/FPS)
 
     ret_physics.location = ground_pos
     ret_physics.velocity = ground_velocity + delta_vel_perp + delta_vel_para
-    ret_physics.angular_velocity = physics.angular_velocity + (F * BALL_RADIUS * delta_vel_para.cross(normal))
+    new_angular = physics.angular_velocity + (F * BALL_RADIUS * delta_vel_para.cross(normal))
+    ret_physics.angular_velocity = new_angular.rescale(omega_max)
+
+    #ret_physics.angular_velocity = physics.angular_velocity + ((BALL_RADIUS / moment_of_inertia) * delta_vel_para_m.cross(normal))
 
     return ret_physics
 
 
-def get_bounces(physics: Physics, bounce_num=5, samples=15):
+def get_bounces(physics: Physics, bounce_num=5, base_samples=15, physics_flag='no_drag'):
     positions = []
+    velocitys = []
+    angulars = []
     times = []
+
     current_physics = physics
     initial_velocity = current_physics.velocity
     initial_position = current_physics.location
     last_time = 0
     for b in range(bounce_num):
-        time_ground = time_to_ground(initial_velocity, initial_position)
+        time_ground = 0
+        if physics_flag == 'no_drag':
+            time_ground = time_to_ground(initial_velocity, initial_position)
+
+        elif physics_flag == 'drag':
+            time_ground = d_time_to_ground(initial_velocity, initial_position)
+
+        elif physics_flag == 'engine_sim':
+            pass
+
+        else:
+            return
+
+        samples = int(base_samples * time_ground)
         time_interval = (time_ground / samples)
+        t = 0
+
         current_pos = None
+        current_vel = None
+        current_ang = None
 
         for s in range(0, samples + 1):
             t = s * time_interval
-            current_pos = get_pos_at_time(initial_velocity, initial_position, t)
+
+            if physics_flag == 'no_drag':
+                current_pos = get_pos_at_time(initial_velocity, initial_position, t)
+                current_vel = get_vel_at_time(t, initial_velocity)
+                current_ang = current_physics.angular_velocity
+
+            elif physics_flag == 'drag':
+                frame = t * FPS
+                current_pos = d_get_pos(frame, initial_velocity, initial_position)
+                current_vel = d_get_vel(frame, initial_velocity)
+                current_ang = current_physics.angular_velocity
+
+            elif physics_flag == 'engine_sim':
+                pass
+
+            else:
+                return
+
             positions.append(current_pos)
+            velocitys.append(current_vel)
+            angulars.append(current_ang)
             times.append(last_time + t)
 
-        new_physics = update_physics_after_bounce(current_physics, time_ground, current_pos)
+        new_physics = update_physics_after_bounce(current_physics, time_ground, current_pos, physics_flag)
 
         initial_velocity = new_physics.velocity
         initial_position = new_physics.location
         current_physics = new_physics
         last_time = last_time + t
 
-    return positions, times
+    return times, positions, velocitys, angulars
 
 
 def burn_time(obj, target_height): # TODO rename to like suicide burn
