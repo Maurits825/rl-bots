44b875a826b35a853301f7e325b26d4ed94c6316
Maurits van der steur <mauritsvdsteur@live.com>
2019-12-24 14:22:40 +0400

    Updated half-flip bot with new rl-gui, works

--- half_flip/half_flip.py
+++ half_flip/half_flip.py
@@ -1,15 +1,32 @@
-from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
-from rlbot.utils.structures.game_data_struct import GameTickPacket
 import math
 import time
+import sys
+import os
+from enum import Enum
+
+from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
+from rlbot.utils.structures.game_data_struct import GameTickPacket
+from rlbot.utils.game_state_util import GameState, BallState, CarState, Physics, Vector3, Rotator, GameInfoState
+
+# in order to access utility folder
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from utility.util import *
-from rlbot.utils.game_state_util import GameState, CarState, Rotator, Physics
-from rlbot.utils.game_state_util import Vector3 as V3
+
+
+class States(Enum):
+    idle = 1
+    reset = 2
+    start = 3
+    first_jump = 4
+    wait_jump = 5
+    second_jump = 6
+    cancel = 7
+    roll = 8
 
 
 class HalfFlip(BaseAgent):
-    def __init__(self, name, team, index):
-        super().__init__(name, team, index)
+
+    def initialize_agent(self):
         self.controller = SimpleControllerState()
 
         # Contants
@@ -18,124 +35,89 @@ class HalfFlip(BaseAgent):
         self.t = 0
 
         self.me = Obj()
-        self.ball = Obj()
 
-        self.current_state = 'Idle'
-        self.next_state = 'Idle'
+        self.current_state = States.idle
+        self.next_state = States.idle
         self.jump_start = 0
 
     def half_flip(self):
-        if self.current_state == 'Idle':
+        if self.current_state is States.idle:
             self.controller.throttle = 0
             self.controller.jump = 0
             self.controller.roll = 0
             self.controller.pitch = 0
-            self.next_state = 'Idle'
+            self.controller.boost = 0
+            self.next_state = States.idle
 
-        elif self.current_state == 'Reset':
+        elif self.current_state is States.reset:
             self.controller.throttle = 0
             self.controller.jump = 0
             self.controller.roll = 0
             self.controller.pitch = 0
-            car_state = CarState(physics=Physics(velocity=V3(0, 0, 0),
+            self.controller.boost = 0
+            car_state = CarState(physics=Physics(velocity=Vector3(0, 0, 0),
                                                  rotation=Rotator(0, 0, 0),
-                                                 angular_velocity=V3(0, 0, 0),
-                                                 location=V3(0, 0, 16.6)))
+                                                 angular_velocity=Vector3(0, 0, 0),
+                                                 location=Vector3(0, 0, 16.6)))
             game_state = GameState(cars={self.index: car_state})
             self.set_game_state(game_state)
-            self.next_state = 'Start'
+            self.next_state = States.start
 
-        elif self.current_state == 'Start':
+        elif self.current_state is States.start:
             self.controller.jump = 1
             self.controller.pitch = 1
+            self.controller.throttle = -1
             self.jump_start = time.time()
-            self.next_state = 'wait_jump'
+            self.next_state = States.wait_jump
 
-        elif self.current_state == 'wait_jump':
+        elif self.current_state is States.wait_jump:
             self.controller.jump = 0
             if (time.time()-self.jump_start) > self.DODGE_TIME:
-                self.next_state = 'second_jump'
+                self.next_state = States.second_jump
             else:
-                self.next_state = 'wait_jump'
+                self.next_state = States.wait_jump
 
-        elif self.current_state == 'second_jump':
+        elif self.current_state is States.second_jump:
             self.controller.jump = 1
             self.controller.pitch = 1
             if self.me.rotation.x > 1.4:
-                self.next_state = 'cancel'
+                self.next_state = States.cancel
             else:
-                self.next_state = 'second_jump'
+                self.next_state = States.second_jump
 
-        elif self.current_state == 'cancel':
+        elif self.current_state is States.cancel:
             self.controller.pitch = -1
             if self.me.rotation.x < 0.3:
-                self.next_state = 'roll'
+                self.next_state = States.roll
             else:
-                self.next_state = 'cancel'
+                self.next_state = States.cancel
 
-        elif self.current_state == 'roll':
+        elif self.current_state is States.roll:
+            self.controller.boost = 1
             self.controller.roll = 1
             if -0.1 < self.me.rotation.z < 0.1:
-                self.next_state = 'Idle'
+                self.next_state = States.idle
             else:
-                self.next_state = 'roll'
+                self.next_state = States.roll
 
         else:
-            self.next_state = 'Idle'
+            self.next_state = States.idle
 
         self.current_state = self.next_state
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         # Update game data variables
-        self.preprocess(packet)
+        self.me = pre_process(packet, self.index)
+        my_car = packet.game_cars[self.index]
 
-        if packet.game_cars[0].jumped and time.time() > self.t+self.timeout:
+        if packet.game_cars[1].jumped and time.time() > self.t+self.timeout:
             self.t = time.time()
-            self.current_state = 'Reset'
+            self.current_state = States.reset
 
-        self.half_flip()
-        # render stuff
         self.renderer.begin_rendering()
-        self.renderer.draw_string_2d(0, 0, 5, 5, self.current_state, self.renderer.black())
-        #self.renderer.draw_string_2d(0, 0, 5, 5, str(packet.game_cars[0].jumped), self.renderer.black())
+        self.renderer.draw_string_3d(my_car.physics.location, 2, 2, str(self.current_state), self.renderer.white())
         self.renderer.end_rendering()
 
+        self.half_flip()
+
         return self.controller
-
-    def preprocess(self, game):
-        index = self.index
-        self.me.pos.x = game.game_cars[index].physics.location.x
-        self.me.pos.y = game.game_cars[index].physics.location.y
-        self.me.pos.z = game.game_cars[index].physics.location.z
-
-        self.me.velocity.x = game.game_cars[index].physics.velocity.x
-        self.me.velocity.y = game.game_cars[index].physics.velocity.y
-        self.me.velocity.z = game.game_cars[index].physics.velocity.z
-
-        self.me.rotation.x = game.game_cars[index].physics.rotation.pitch
-        self.me.rotation.y = game.game_cars[index].physics.rotation.yaw
-        self.me.rotation.z = game.game_cars[index].physics.rotation.roll
-
-        self.me.rvel.x = game.game_cars[index].physics.angular_velocity.x
-        self.me.rvel.y = game.game_cars[index].physics.angular_velocity.y
-        self.me.rvel.z = game.game_cars[index].physics.angular_velocity.z
-
-        self.ball.pos.x = game.game_ball.physics.location.x
-        self.ball.pos.y = game.game_ball.physics.location.y
-        self.ball.pos.z = game.game_ball.physics.location.z
-
-        self.ball.velocity.x = game.game_ball.physics.velocity.x
-        self.ball.velocity.y = game.game_ball.physics.velocity.y
-        self.ball.velocity.z = game.game_ball.physics.velocity.z
-
-        self.ball.rotation.x = game.game_ball.physics.rotation.pitch
-        self.ball.rotation.y = game.game_ball.physics.rotation.yaw
-        self.ball.rotation.z = game.game_ball.physics.rotation.roll
-
-        self.ball.rvel.x = game.game_ball.physics.angular_velocity.x
-        self.ball.rvel.y = game.game_ball.physics.angular_velocity.y
-        self.ball.rvel.z = game.game_ball.physics.angular_velocity.z
-
-        #self.me.matrix = rotator_to_matrix(self.me)
-
-        self.ball.isBall = True
